6baa5fbcbd633083209118f0f6f6fbc0cc14f7ec
GovindKurmi <govind187250@gmail.com>
2022-06-18 15:02:57 +0530

    first commit

---
+++ src/main/java/com/gk/vehical/VehicalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.gk.vehical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicalsystemApplication.class, args);
+	}
+
+}
